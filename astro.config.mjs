d81a70331ff92c7470fd7b10e25f1cabacca23de
y-takagiiii <takagiyuri0712@gmail.com>
2024-07-14 13:26:45 +0900

    全ページでSSRさせる

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: "hybrid",
+  output: "server",
   adapter: cloudflare(),
 });
